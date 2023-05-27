fe465b158c18c5097c149bab31836bfdd277a81a
unknown <alexandre_2420@outlook.com>
2023-05-27 09:17:28 -0500

    section 2: 10.Agregar nuevas animaciones

--- Assets/Scripts/PersonajeAnimaciones.cs
+++ Assets/Scripts/PersonajeAnimaciones.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PersonajeAnimaciones : MonoBehaviour
 {
+    [SerializeField] private string layerIdle;
+    [SerializeField] private string layerCaminar;
     private Animator _animator;
     private PersonajeMovimiento _personajeMovimiento;
     private readonly int direccionX = Animator.StringToHash("x");
@@ -18,6 +18,8 @@ public class PersonajeAnimaciones : MonoBehaviour
 
     private void Update()
     {
+        ActualizarLayers();
+
         // cuando nos dejemos de mover, se quedara la ultima animacion aplicada
         // y no volveremos a la animacion inicial
         if (!_personajeMovimiento.EnMovimiento)
@@ -27,5 +29,28 @@ public class PersonajeAnimaciones : MonoBehaviour
 
         _animator.SetFloat(direccionX, _personajeMovimiento.DireccionMovimiento.x);
         _animator.SetFloat(direccionY, _personajeMovimiento.DireccionMovimiento.y);
+    }
+
+    private void ActualizarLayers()
+    {
+        if (_personajeMovimiento.EnMovimiento)
+        {
+            ActivarLayer(layerCaminar);
+        }
+        else
+        {
+            ActivarLayer(layerIdle);
+        }
+    }
+
+    private void ActivarLayer(string nombreLayer)
+    {
+        for (int i = 0; i < _animator.layerCount; i++)
+        {
+            // estamos desactivando los leyers (0 => desactivado, 1 => activado)
+            _animator.SetLayerWeight(i, 0);
+        }
+        // activamos el layer 
+        _animator.SetLayerWeight(_animator.GetLayerIndex(nombreLayer), 1);
     }
 }
