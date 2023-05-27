5080e8ca88429736f4685511cd089554437c059b
unknown <alexandre_2420@outlook.com>
2023-05-27 14:53:14 -0500

    section 2: 17.Panel de stats

---
+++ Assets/Scripts/Personaje/PersonajeStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stats")]
+public class PersonajeStats : ScriptableObject
+{
+    [Header("Stats")]
+    public float Daño = 5f;
+    public float Defensa = 2f;
+    public float Velocidad = 5f;
+    public float Nivel;
+    public float ExpActual;
+    public float ExpRequeridaSiguienteNivel;
+    [Range(0f, 100f)] public float PorcentajeCritico;
+    [Range(0f, 100f)] public float PorcentajeBloqueo;
+}
