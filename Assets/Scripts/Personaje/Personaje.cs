35b7ba56b87a3083c8421fc57301e51436273410
unknown <alexandre_2420@outlook.com>
2023-05-27 17:13:51 -0500

    section 3: 22. Trabajar con Tilemaps

--- Assets/Scripts/Personaje/Personaje.cs
+++ Assets/Scripts/Personaje/Personaje.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class Personaje : MonoBehaviour
@@ -60,5 +57,4 @@ public class Personaje : MonoBehaviour
 
         stats.PuntosDisponibles -= 1;
     }
-
 }
