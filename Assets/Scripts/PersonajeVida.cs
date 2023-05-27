f120c1a44f07d9b5232fde0538700a1ae2e5a7d3
unknown <alexandre_2420@outlook.com>
2023-05-27 10:08:30 -0500

    section 2: 11.Añadir vida al personaje

---
+++ Assets/Scripts/PersonajeVida.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PersonajeVida : VidaBase 
+{
+    public bool PuedeSerCurado => Salud < saludMax;
+    private BoxCollider2D _boxCollider2D;
+
+    private void Awake()
+    {
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        ActualizarBarraVida(Salud, saludMax);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            RecibirDaño(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            RestaurarSalud(10);
+        }
+    }
+
+    public void RestaurarSalud(float cantidad)
+    {
+        if (PuedeSerCurado)
+        {
+            Salud += cantidad;
+            if (Salud > saludMax)
+            {
+                Salud = saludMax;
+            }
+
+            ActualizarBarraVida(Salud, saludMax);
+        }
+    }
+
+    protected override void PersonajeDerrotado()
+    {
+      
+    }
+
+    protected override void ActualizarBarraVida(float vidaActual, float vidaMax)
+    {
+       
+    }
+
+}
---
+++ Assets/Scripts/VidaBase.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class VidaBase : MonoBehaviour
+{
+    [SerializeField] protected float saludInicial;
+    [SerializeField] protected float saludMax;
+
+    public float Salud { get; protected set; }
+
+    protected virtual void Start()
+    {
+        Salud = saludInicial;
+    }
+
+    public void RecibirDaño(float cantidad)
+    {
+        if (cantidad <= 0)
+        {
+            return;
+        }
+
+        if (Salud > 0f)
+        {
+            Salud -= cantidad;
+            ActualizarBarraVida(Salud, saludMax);
+            if (Salud <= 0f)
+            {
+                ActualizarBarraVida(Salud, saludMax);
+                PersonajeDerrotado();
+            }
+        }
+    }
+
+    protected virtual void ActualizarBarraVida(float vidaActual, float vidaMax)
+    {
+
+    }
+
+    protected virtual void PersonajeDerrotado()
+    {
+
+    }
+}
