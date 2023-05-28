a5e44f64ccd6efcfa6773b016d08bafbf8d33bcc
unknown <alexandre_2420@outlook.com>
2023-05-28 11:12:06 -0500

    section 3: 28. Crear inventario UI

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -9,6 +9,7 @@ public class UIManager : Singleton<UIManager>
 
     [Header("Paneles")]
     [SerializeField] private GameObject panelStats;
+    [SerializeField] private GameObject panelInventario;
 
     [Header("Barra")]
     [SerializeField] private Image vidaPlayer;
@@ -103,4 +104,16 @@ public class UIManager : Singleton<UIManager>
         expActual = pExpActual;
         expRequeridaNuevoNivel = pExpRequerida;
     }
+
+    #region Paneles
+    public void AbrirCerrarPanelStats()
+    {
+        panelStats.SetActive(!panelStats.activeSelf);
+    }
+
+    public void AbrirCerrarPanelInventario()
+    {
+        panelInventario.SetActive(!panelInventario.activeSelf);
+    }
+    #endregion
 }
