7157e16a4a357d28e492855631455ab7b01e8c5e
unknown <alexandre_2420@outlook.com>
2023-05-28 11:44:55 -0500

    section 3: 29. Añadir Slots al inventario

---
+++ Assets/Scripts/Inventario/Inventario.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : Singleton<Inventario>
+{
+    [Header("Items")]
+    [SerializeField] private InventarioItem[] itemsInventario;
+    //[SerializeField] private Personaje personaje;
+    [SerializeField] private int numeroDeSlots;
+
+    //public Personaje Personaje => personaje;
+    public int NumeroDeSlots => numeroDeSlots;
+    public InventarioItem[] ItemsInventario => itemsInventario;
+
+    private void Start()
+    {
+        itemsInventario = new InventarioItem[numeroDeSlots];
+    }
+
+}
